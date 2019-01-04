f9efb5caf7671ae56f715a977c5db1fed26821e7
Jamith Nimantha <jamithnimantha@gmail.com>
2019-01-05 01:08:18 +0530

    laucher icon added

--- app/src/main/java/com/jamith/opentorch/MainActivity.java
+++ app/src/main/java/com/jamith/opentorch/MainActivity.java
@@ -3,16 +3,15 @@ package com.jamith.opentorch;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -91,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             if (grantResults.length>0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
                 imgBtnSwitch.setEnabled(false);
                 imageViewStatus.setEnabled(true);
+
             }else {
                 Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_SHORT).show();
             }
